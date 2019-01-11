8449af05e4512f31e8c5e711668b302821de085f
2908819136 <2908819136@qq.com>
2019-01-11 12:35:31 +0800

    20190111123523

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: "Gatsby + Netlify CMS Starter",
+        title: "上海匡架网络",
         description:
-            "It is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth."
+            "上海匡架网络科技有限公司作为领先的移动互联网应用服务提供商，致力于将先进的营销理念、客户服务理念和通信信息服务平台相结合，为企业提供基于移动互联网的创新的应用解决方案与服务。帮助企业利用移动互联网和客户、员工、合作伙伴更有效地沟通互动，创造营销、客服新价值。"
     },
     plugins: [
         "gatsby-plugin-typescript",
